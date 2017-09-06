c53e11ca04d6e17426f245137c66a2052ab9753f
zain <zain@zain-PC>
2017-09-07 00:41:24 +0530

    start1

--- Sele_frme/src/driver/test.java
+++ Sele_frme/src/driver/test.java
@@ -3,19 +3,16 @@ package driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test {
+import framewrk.frme_wrk;
+
+public class test extends frme_wrk{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\zain\\git\\Selenium\\Sele_frme\\addl_req\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://www.google.com");
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		sleep(10000);
 		driver.close();
 	}
 
---
+++ Sele_frme/src/framewrk/frme_wrk.java
@@ -0,0 +1,13 @@
+package framewrk;
+
+public class frme_wrk {
+
+	public static void sleep(int millis){
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
