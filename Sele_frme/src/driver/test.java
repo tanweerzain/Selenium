30371eddbeab70897d7423fd930348dc1a6a585e
zain <zain@zain-PC>
2017-09-07 02:21:50 +0530

    mouse hover and click added to framework.

--- Sele_frme/src/driver/test.java
+++ Sele_frme/src/driver/test.java
@@ -1,19 +1,27 @@
 package driver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import framewrk.frme_wrk;
 
-public class test extends frme_wrk{
+public class test{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\zain\\git\\Selenium\\Sele_frme\\addl_req\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.navigate().to("http://www.google.com");
-		sleep(10000);
+		frme_wrk zzz = new frme_wrk(driver);
+		driver.navigate().to("http://www.amazon.in");
+		zzz.mouseHover("//*[@id='nav-link-yourAccount']");
+		zzz.sleep(10000);
+		//zzz.click("//*[@id='nav-flyout-ya-signin']/a/span");
+		zzz.click("//*[@id='nav-flyout-ya-newCust']/a");
+		zzz.sleep(10000);
 		driver.close();
+		//*[@id="nav-flyout-ya-signin"]/a/span
 	}
 
 }
--- Sele_frme/src/framewrk/frme_wrk.java
+++ Sele_frme/src/framewrk/frme_wrk.java
@@ -1,6 +1,20 @@
 package framewrk;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class frme_wrk {
+
+	WebDriver driver;
+
+	public frme_wrk(WebDriver driver) {
+		super();
+		this.driver = driver;
+	}
 
 	public static void sleep(int millis){
 		try {
@@ -8,6 +22,58 @@ public class frme_wrk {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	public void mouseHover(String xpath) {
+		WebElement HoverElement = driver.findElement(By.xpath(xpath));
+		try {
+			if (isElementPresent(HoverElement)) {
+				
+				String mouseHoverScript = "if(document.createEvent){var evObj = document.createEvent('MouseEvents');evObj.initEvent('mouseover', true, false); arguments[0].dispatchEvent(evObj);} else if(document.createEventObject) { arguments[0].fireEvent('onmouseover');}";
+				
+				((JavascriptExecutor) driver).executeScript(mouseHoverScript,HoverElement);
+				//
+			} else {
+				System.out.println("Element was not visible \n");
+
+			}
+		} catch (StaleElementReferenceException e) {
+			System.out.println("Element with " + HoverElement
+					+ "is not attached to the page document"
+					+ e.getStackTrace());
+		} catch (NoSuchElementException e) {
+			System.out.println("Element " + HoverElement + " was not found on page"
+					+ e.getStackTrace());
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Error occurred while hovering"
+					+ e.getStackTrace());
+		}
+	}
+
+	public boolean isElementPresent(WebElement element) {
+		boolean present = false;
+		try {
+			if (element.isDisplayed()
+					|| element.isEnabled())
+				present = true;
+		} catch (NoSuchElementException e) {
+			present = false;
+		} catch (StaleElementReferenceException e) {
+			present = false;
+		}
+		return present;
+	}
+	
+	public void click(String xpath){
+		WebElement testElement = driver.findElement(By.xpath(xpath));
+		if (isElementPresent(testElement)) {
+			testElement.click();
+			//
+		} else {
+			System.out.println("Element was not visible \n");
+
 		}
 	}
 }
