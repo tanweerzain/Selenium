ec79a7ea9cc82aa8735ff95451f543752f44dfd8
zain <zain@zain-PC>
2017-09-09 05:31:37 +0530

    extending framework for basic operations and logging. Setting up driver for website.

--- Sele_frme/src/driver/test.java
+++ Sele_frme/src/driver/test.java
@@ -1,9 +1,16 @@
 package driver;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import framewrk.frme_wrk;
 
@@ -11,17 +18,37 @@ public class test{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\zain\\git\\Selenium\\Sele_frme\\addl_req\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		frme_wrk zzz = new frme_wrk(driver);
-		driver.navigate().to("http://www.amazon.in");
-		zzz.mouseHover("//*[@id='nav-link-yourAccount']");
-		zzz.sleep(10000);
-		//zzz.click("//*[@id='nav-flyout-ya-signin']/a/span");
+
+		frme_wrk zzz = new frme_wrk("Amazon");
+		zzz.gotourl("http://www.amazon.in");
+		zzz.logger("first page", true);
+        //zzz.sleep(10000);
+        /*List<WebElement> elements = driver.findElements(By.cssSelector("*"));
+        System.out.println("ghjhgkj "+elements.size());
+        for(WebElement ele:elements)
+        {
+
+                System.out.println(ele.getAttribute("id"));// + " - " + ele.getText());              //for getting text of each element
+
+        }
+        System.exit(0);*/
+		//zzz.mouseHover("//*[@id='nav-link-accountList']");
+		zzz.mouseHoveract("//*[@id='nav-tools']/a[1]");
+		zzz.logger("menu", true);
 		zzz.click("//*[@id='nav-flyout-ya-newCust']/a");
-		zzz.sleep(10000);
-		driver.close();
-		//*[@id="nav-flyout-ya-signin"]/a/span
+		zzz.logger("blank registration page", true);
+		zzz.senddata("//*[@id='ap_customer_name']", "zain341");
+		zzz.click("//*[@id='auth-country-picker-container']/span");
+		//zzz.click("//*[@id='auth-country-picker_90']");
+		zzz.click("//*[@id='auth-country-picker_89']");
+		zzz.senddata("//*[@id='ap_phone_number']", "89874983453");
+		zzz.senddata("//*[@id='ap_email']","assddadsf@gmail.com");
+		zzz.senddata("//*[@id='ap_password']", "Qwerty123");
+		zzz.logger("filled registration page", true);
+		//zzz.click("//*[@id='continue']");
+		
+		zzz.sleep(5000);
+		zzz.closebrowser();
 	}
 
 }
--- Sele_frme/src/framewrk/frme_wrk.java
+++ Sele_frme/src/framewrk/frme_wrk.java
@@ -2,21 +2,94 @@ package framewrk;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.google.common.io.Files;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.WebDriver;
+
 public class frme_wrk {
 
 	WebDriver driver;
+	Logger logger;  
 
-	public frme_wrk(WebDriver driver) {
+	public static long stepno=-2;
+	
+	public frme_wrk(String testcase) {
 		super();
-		this.driver = driver;
+		startlogging(testcase);
+		logger("Setting up and launching Chrome browser...", false);
+		this.driver = getdriver();
+		logger("Beginning TestCase: " + testcase, false);
+
 	}
 
-	public static void sleep(int millis){
+	public void startlogging(String testcase){
+		logger = Logger.getLogger(testcase);
+	    FileHandler fh;  
+	    try {  
+
+	    	Path pathToFile = Paths.get("C:/Users/zain/desktop/test/MyLogFile.log");
+	        //Files.createDirectories(pathToFile.getParent());
+	        File parentDir = new File(pathToFile.getParent().toString());
+          if(!parentDir.exists()) parentDir.mkdirs();
+	        
+	        // This block configure the logger with handler and formatter  
+	        fh = new FileHandler("C:/Users/zain/desktop/test/MyLogFile.log"); 
+	        logger.addHandler(fh);
+	        
+	        SimpleFormatter formatter = new SimpleFormatter();
+	        fh.setFormatter(formatter);  
+
+	    } catch (SecurityException e) {  
+	        e.printStackTrace();  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    }  
+		this.logger = logger;
+	}
+	public WebDriver getdriver(){
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\zain\\git\\Selenium\\Sele_frme\\addl_req\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		return driver;
+	}
+	
+	public void closebrowser(){
+		driver.close();
+	}
+
+	public void gotourl(String url){
+		driver.navigate().to(url);
+	}
+	
+	public void sleep(int millis){
 		try {
 			Thread.sleep(millis);
 		} catch (InterruptedException e) {
@@ -26,49 +99,73 @@ public class frme_wrk {
 	}
 	
 	public void mouseHover(String xpath) {
-		WebElement HoverElement = driver.findElement(By.xpath(xpath));
-		try {
-			if (isElementPresent(HoverElement)) {
+		WebElement HoverElement = isElementPresent(xpath);
+			if (HoverElement != null) {
 				
 				String mouseHoverScript = "if(document.createEvent){var evObj = document.createEvent('MouseEvents');evObj.initEvent('mouseover', true, false); arguments[0].dispatchEvent(evObj);} else if(document.createEventObject) { arguments[0].fireEvent('onmouseover');}";
 				
 				((JavascriptExecutor) driver).executeScript(mouseHoverScript,HoverElement);
-				//
+				new Actions(driver).pause(1000000);
 			} else {
 				System.out.println("Element was not visible \n");
-
 			}
-		} catch (StaleElementReferenceException e) {
-			System.out.println("Element with " + HoverElement
-					+ "is not attached to the page document"
-					+ e.getStackTrace());
-		} catch (NoSuchElementException e) {
-			System.out.println("Element " + HoverElement + " was not found on page"
-					+ e.getStackTrace());
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("Error occurred while hovering"
-					+ e.getStackTrace());
-		}
 	}
 
-	public boolean isElementPresent(WebElement element) {
+	public void mouseHoveract(String xpath) {
+		WebElement HoverElement = isElementPresent(xpath);
+			if (HoverElement != null) {
+				Actions action = new Actions(driver);
+				try {
+					new Robot().mouseMove(0, 0);
+				} catch (AWTException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}	
+		        action.moveToElement(HoverElement).build().perform();
+				//action.pause(1000000);
+			} else {
+				System.out.println("Element was not visible \n");
+			}
+	}
+	
+	public WebElement isElementPresent(String xpath) {
 		boolean present = false;
+		//new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(ExpectedConditions.ElementExists((By.Id(login))));
+		Wait<WebDriver> wait = new FluentWait<WebDriver> (driver).withTimeout(60, TimeUnit.SECONDS).pollingEvery(2, TimeUnit.SECONDS).ignoring(NoSuchElementException.class,ElementNotVisibleException.class);
+		WebElement findElement = null;
 		try {
-			if (element.isDisplayed()
-					|| element.isEnabled())
+//			WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+//					  .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+			findElement = wait.until(new Function<WebDriver, WebElement>() {
+		        public WebElement apply(WebDriver webDriver) {
+		            return driver.findElement(By.xpath(xpath));}
+		    });
+			
+			//HoverElement = driver.findElement(By.xpath(xpath));
+			if (findElement.isDisplayed()
+					|| findElement.isEnabled())
 				present = true;
-		} catch (NoSuchElementException e) {
-			present = false;
 		} catch (StaleElementReferenceException e) {
 			present = false;
+			System.out.println("Element with " + xpath
+				+ "is not attached to the page document"
+				+ e.getStackTrace());
+		} catch (NoSuchElementException e) {
+			present = false;
+			System.out.println("Element " + xpath + " was not found on page"
+				+ e.getStackTrace());
+		} catch (Exception e) {
+			present = false;
+			e.printStackTrace();
+			System.out.println("Error occurred while hovering " + xpath 
+				+ e.getStackTrace());
 		}
-		return present;
+		return present?findElement:null;
 	}
 	
 	public void click(String xpath){
-		WebElement testElement = driver.findElement(By.xpath(xpath));
-		if (isElementPresent(testElement)) {
+		WebElement testElement = isElementPresent(xpath);
+		if (testElement != null) {
 			testElement.click();
 			//
 		} else {
@@ -76,4 +173,38 @@ public class frme_wrk {
 
 		}
 	}
+	
+	public void logger(String stepname, boolean takeshot){
+		stepno++;
+		
+	    logger.info("Step" + (stepno+2) + ": " + stepname);
+	    if (takeshot)
+	    {
+	    	File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	    	// Now you can do whatever you need to do with it, for example copy somewhere
+	    	try {
+	    		Files.copy(scrFile, new File("C:/Users/zain/desktop/test/" + "Step" + stepno + ".png"));
+	    	} catch (IOException e) {
+	    	// TODO Auto-generated catch block
+	    		e.printStackTrace();
+	    	}
+	    }
+	}
+	
+	public void senddata(String xpath, String data){
+		WebElement testElement = isElementPresent(xpath);
+		if (testElement != null) {
+			testElement.sendKeys(data);
+			//
+		} else {
+			System.out.println("Element was not visible \n");
+
+		}
+	}
+	
+	public void boxselect(String xpath, String value){
+		Select ss = new Select(isElementPresent(xpath));
+		ss.selectByVisibleText(value);
+	}
+	
 }
